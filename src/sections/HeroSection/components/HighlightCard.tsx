544979174bb2294d85a4b36d1aa0d7329286c348
oliveiradniel <kadadniel@gmail.com>
2025-06-19 12:58:53 -0300

    style: add horizontal infinite scroll animation to highlight list

--- src/sections/HeroSection/components/HighlightCard.tsx
+++ src/sections/HeroSection/components/HighlightCard.tsx
@@ -2,9 +2,9 @@ import type { HighlightCardProps } from "../../../@types/HighlightItem";
 
 export default function HighlightCard({ Icon, text }: HighlightCardProps) {
   return (
-    <div className="flex gap-4 text-[14px] items-center">
-      <Icon aria-hidden="true" size={42} color="#87ceeb" />
-      <p className="flex-1 md:max-w-[280px]">{text}</p>
+    <div className="flex gap-3 text-[14px] items-center min-w-max">
+      <Icon aria-hidden="true" size={38} color="#87ceeb" />
+      <p className="flex-1 max-w-[280px]">{text}</p>
     </div>
   );
 }
--- src/sections/HeroSection/components/HighlightList.tsx
+++ src/sections/HeroSection/components/HighlightList.tsx
@@ -4,9 +4,16 @@ import HighlightCard from "./HighlightCard";
 
 export default function HighlightList() {
   return (
-    <div className="border border-x-0 py-4 flex border-sky-blue flex-col gap-6 md:gap-3 shadow-[0_4px_6px_-4px_rgba(0,0,0,0.3)] md:flex-row">
-      {heroHighlights.map(({ Icon, text }, index) => (
-        <HighlightCard key={index} Icon={Icon} text={text} />
+    <div className="border border-x-0 py-4 flex gap-4 border-sky-blue shadow-[0_4px_6px_-4px_rgba(0,0,0,0.3)] overflow-hidden max-w-[900px] mask-x-from-90%">
+      {[...Array(3)].map((_, index) => (
+        <div
+          key={index}
+          className="flex md:flex-nowrap animate-infinite-horizontal-scroll gap-4"
+        >
+          {heroHighlights.map(({ Icon, text }, index) => (
+            <HighlightCard key={index} Icon={Icon} text={text} />
+          ))}
+        </div>
       ))}
     </div>
   );
