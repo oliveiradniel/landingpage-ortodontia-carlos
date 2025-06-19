7c6950dd4da28f0e1710f3c1b5c7c1a99a919b0a
oliveiradniel <kadadniel@gmail.com>
2025-06-19 16:49:45 -0300

    fix: remove the mask generator function for tailwindcss to understand it correctly

--- src/assets/styles/underlines.ts
+++ src/assets/styles/underlines.ts
@@ -1,16 +1,7 @@
-const horizontalFadeMask = (pseudoElement: "after" | "before") =>
-  `${pseudoElement}:mask-[linear-gradient(to_right,transparent,black_40%,black_60%,transparent)]`;
+export const h2BottomUnderline = `after:absolute after:w-[40%] after:h-[1.4px] after:bg-ocean-blue after:left-1/2 after:-translate-x-1/2 after:right-0 after:bottom-[-8px] after:mask-[linear-gradient(to_right,transparent,black_40%,black_60%,transparent)]`;
 
-export const h2BottomUnderline = `after:absolute after:w-[40%] after:h-[1.4px] after:bg-ocean-blue after:left-1/2 after:-translate-x-1/2 after:right-0 after:bottom-[-8px] ${horizontalFadeMask(
-  "after"
-)}`;
+export const topUnderline = `before:absolute before:w-full before:h-[1.4px] before:bg-ocean-blue before:left-1/2 before:-translate-x-1/2 before:right-0 before:top-0 before:mask-[linear-gradient(to_right,transparent,black_40%,black_60%,transparent)]`;
 
-export const topUnderline = `before:absolute before:w-full before:h-[1.4px] before:bg-ocean-blue before:left-1/2 before:-translate-x-1/2 before:right-0 before:top-0 ${horizontalFadeMask(
-  "before"
-)}`;
-
-export const bottomUnderline = `after:absolute after:w-full after:h-[2px] after:bg-ocean-blue after:left-1/2 after:-translate-x-1/2 after:right-0 after:bottom-0 ${horizontalFadeMask(
-  "after"
-)}`;
+export const bottomUnderline = `after:absolute after:w-full after:h-[2px] after:bg-ocean-blue after:left-1/2 after:-translate-x-1/2 after:right-0 after:bottom-0 after:mask-[linear-gradient(to_right,transparent,black_40%,black_60%,transparent)]`;
 
 export const doubleUnderline = `${topUnderline} ${bottomUnderline}`;
