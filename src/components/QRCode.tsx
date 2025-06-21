a97f14dd34e4b1a61bb7683a63644f72f4788542
oliveiradniel <kadadniel@gmail.com>
2025-06-21 17:58:41 -0300

    style: turn off events in tooltip div

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -2,7 +2,7 @@ import qrCode from "@/assets/images/qr-code.png";
 
 export default function QRCode() {
   return (
-    <div className="absolute flex flex-col border w-[200px] h-auto p-2 border-black/40 bottom-[120%] right-1/2 translate-x-[50%] bg-white">
+    <div className="absolute rounded-md flex flex-col border w-[200px] h-auto p-2 border-black/20 bottom-[120%] right-1/2 translate-x-[50%] bg-white">
       <img src={qrCode} alt="QR Code para ir até o WhatsApp" />
       <small className="text-center text-gray-600">Escanear o código</small>
     </div>
--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -6,7 +6,7 @@ interface TooltipProps {
 
 export default function Tooltip({ message }: TooltipProps) {
   return (
-    <div className="absolute border border-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out rounded-s-full rounded-tr-full top-[-100%] shadow-[4px_6px_12px_0px_rgba(0,0,0,0.1)] bg-white text-black px-4 py-1 translate-x-[-85%]">
+    <div className="absolute border border-black/20 opacity-0 group-hover:opacity-100 pointer-events-none transition-opacity duration-300 ease-in-out rounded-s-full rounded-tr-full top-[-100%] shadow-[4px_6px_12px_0px_rgba(0,0,0,0.1)] bg-white text-black px-4 py-1 translate-x-[-85%]">
       {message === "Posso ajudar?" && <QRCode />}
 
       <p className="whitespace-nowrap">{message}</p>
